bae64f982cdddade5060ed47e556386b658ca078
polinalepehina@gmail.com <polinalepehina@gmail.com>
2020-03-10 20:00:59 +0200

    Добавьте файлы проекта.

---
+++ Project1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите а");
+        double a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите в");
+        double b = double.Parse(Console.ReadLine());
+        double c = a / b;
+        Console.WriteLine("Результат с = " + c);
+        Console.ReadKey();
+
+    }
+}
